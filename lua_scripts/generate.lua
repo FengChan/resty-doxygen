41d32c332a29466f9faf3718fec5c5701a3dd840
呱呱呱 <chenjing19930415@163.com>
2025-05-29 20:30:17 +0800

    fix

--- lua_scripts/generate.lua
+++ lua_scripts/generate.lua
@@ -70,12 +70,11 @@ local cmd = string.format([[
     sed -i 's/CALLER_GRAPH.*/CALLER_GRAPH = YES/' Doxyfile && \
     echo "" > footer.html && \
     echo "body#top > * {display: none !important;}" > custom.css && \
-    sed -i 's|HTML_FOOTER.*|HTML_FOOTER = footer.html|' Doxyfile && \
+    sed -i 's/HTML_FOOTER.*/HTML_FOOTER = footer.html/' Doxyfile && \
+    sed -i 's/HTML_EXTRA_STYLESHEET.*/HTML_EXTRA_STYLESHEET = custom.css/' Doxyfile && \
     sed -i 's/CLASS_DIAGRAMS.*/CLASS_DIAGRAMS = YES/' Doxyfile && \
     sed -i 's/SEARCHENGINE.*/SEARCHENGINE = NO/' Doxyfile && \
     sed -i 's/DOT_GRAPH_MAX_NODES.*/DOT_GRAPH_MAX_NODES = 100/' Doxyfile && \
-    sed -i 's|HTML_EXTRA_STYLESHEET.*|HTML_EXTRA_STYLESHEET = custom.css|' Doxyfile &&\
-    echo 'HTML_EXTRA_STYLESHEET = custom.css' >> Doxyfile &&\
     
     doxygen Doxyfile && \
     mkdir -p %s && \
