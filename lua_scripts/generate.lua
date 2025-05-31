0e0d3f6f6ca853b44f3cd0850bc9e6e8786ec167
呱呱呱 <chenjing19930415@163.com>
2025-05-31 15:14:19 +0800

    test

--- lua_scripts/generate.lua
+++ lua_scripts/generate.lua
@@ -62,7 +62,7 @@ local cmds = {
     string.format("git clone %s %s", repo, repopath),
     string.format("cd %s", repopath),
     "doxygen -g",
-    "sed -i 's/PROJECT_NAME.*/PROJECT_NAME = \"gogogo\"/' Doxyfile",
+    "sed -i 's/^PROJECT_NAME.*/PROJECT_NAME = \"gogogo\"/' Doxyfile",
     "sed -i 's/EXTRACT_ALL.*/EXTRACT_ALL = YES/' Doxyfile",
     "sed -i 's/GENERATE_LATEX.*/GENERATE_LATEX = NO/' Doxyfile",
     "sed -i 's/HAVE_DOT.*/HAVE_DOT = YES/' Doxyfile",
